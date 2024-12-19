6241e6c15ce8d50c62613ab70b0fc094b2af78b4
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-12-19 20:56:00 +0100

    fix: include missing index html file (#13)

--- src/parts/Root/Root.ts
+++ src/parts/Root/Root.ts
@@ -2,4 +2,5 @@ import { dirname, join } from 'node:path'
 import { fileURLToPath } from 'node:url'
 
 const __dirname = dirname(fileURLToPath(import.meta.url))
-export const root = join(__dirname, '../../..')
+
+export const root = join(__dirname, '..')
