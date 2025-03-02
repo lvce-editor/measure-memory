3ce17e2ce399d3ffe3743f671388e8d803aad8a4
Le Vivilet <levivlet@pm.me>
2025-03-02 17:00:16 +0100

    fix: types

--- src/main.d.ts
+++ src/main.d.ts
@@ -2,6 +2,8 @@ interface Options {
   readonly workerPath: string
   readonly playwrightPath: string
   readonly threshold: number
+  readonly instantiations?: number
+  readonly instantiationsPath?: string
 }
 
 export const measureMemory: (options: Options) => Promise<void>
