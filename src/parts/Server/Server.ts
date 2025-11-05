1b3077aaa55cf704936264693e40da61f679fe33
Le Vivilet <levivilet@pm.me>
2025-11-05 17:25:50 +0100

    feature: simplify server start (#65)

--- src/parts/Server/Server.ts
+++ src/parts/Server/Server.ts
@@ -1,18 +1,22 @@
 import type { Server } from 'node:http'
 import { createApp } from '../CreateApp/CreateApp.ts'
 
+const listen = async (app: any, port: number): Promise<Server> => {
+  const { promise, resolve } = Promise.withResolvers<void>()
+  const server = app.listen(port, () => {
+    resolve()
+  })
+  await promise
+  return server
+}
+
 export const startServer = async (
   port: number,
   workerPath: string,
   root: string,
 ): Promise<Server> => {
   const app = createApp(workerPath, root)
-
-  const { promise, resolve } = Promise.withResolvers<void>()
-  const server = app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`)
-    resolve()
-  })
-  await promise
+  const server = await listen(app, port)
+  console.log(`Server running at http://localhost:${port}`)
   return server
 }
