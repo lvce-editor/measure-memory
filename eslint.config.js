ffcf01c5f826affe8b5135ffc4dd47e0c8dab5ca
Le Vivilet <levivilet@pm.me>
2025-11-19 16:51:22 +0100

    lint

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,9 @@
 import * as config from '@lvce-editor/eslint-config'
+import * as actions from '@lvce-editor/eslint-plugin-github-actions'
 
 export default [
   ...config.default,
+  ...actions.default,
   {
     ignores: [
       'packages/text-search-worker/src/textSearchWorkerMain.ts',
