be8a55d7eaced26f8dbcfa54cbfee261f57a6fd2
Le Vivilet <levivilet@pm.me>
2025-11-20 00:03:03 +0100

    fix: missing content type (#74)

--- src/parts/CreateApp/CreateApp.ts
+++ src/parts/CreateApp/CreateApp.ts
@@ -8,9 +8,11 @@ export const createApp = (aboutWorkerPath: string, root: string): Server => {
   const handleRequest = async (req: IncomingMessage, res: ServerResponse) => {
     switch (req.url) {
       case '/':
+        res.setHeader('Content-Type', 'text/html')
         await pipeline(createReadStream(join(root, 'index.html')), res)
         break
       case '/dist/aboutWorkerMain.js':
+        res.setHeader('Content-Type', 'application/javascript')
         await pipeline(createReadStream(aboutWorkerPath), res)
         break
       default:
